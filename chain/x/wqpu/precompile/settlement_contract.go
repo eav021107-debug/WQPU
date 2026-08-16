6b276a6b14b3967709e9c8602f3465df329bebde
eav021107-debug <eav021107@gmail.com>
2026-08-16 21:53:30 +0300

    Use bonded price epochs in WQPU runtime

--- chain/x/wqpu/precompile/settlement_contract.go
+++ chain/x/wqpu/precompile/settlement_contract.go
@@ -110,6 +110,6 @@ func (c SettlementNetworkContract) Run(evm *vm.EVM, contract *vm.Contract, readO
 func WithWQPUSettlementNetwork(existing map[common.Address]vm.PrecompiledContract) map[common.Address]vm.PrecompiledContract {
 	if existing == nil { existing = make(map[common.Address]vm.PrecompiledContract) }
 	if current, exists := existing[Address]; exists { panic("WQPU precompile address collision with " + current.Name()) }
-	existing[Address] = NewBondedSettlementNetworkContract(DevNetworkConfig)
+	existing[Address] = NewPricedSettlementNetworkContract(DevNetworkConfig)
 	return existing
 }
