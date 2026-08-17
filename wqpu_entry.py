e3afa49e7c2ff9a7172949ce258d5321011ebcbb
eav021107-debug <eav021107@gmail.com>
2026-08-17 13:42:53 +0300

    Add unified WQPU command entrypoint

---
+++ wqpu_entry.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Unified WQPU command entrypoint."""
+
+from __future__ import print_function
+
+import sys
+
+
+def main():
+    if len(sys.argv) > 1 and sys.argv[1].lower() == "claim":
+        import wqpu_claim
+        sys.argv = [sys.argv[0]] + sys.argv[2:]
+        return wqpu_claim.main()
+
+    import wqpu_runtime
+    return wqpu_runtime.main()
+
+
+if __name__ == "__main__":
+    raise SystemExit(main())
